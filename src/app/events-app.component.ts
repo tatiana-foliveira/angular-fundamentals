0bddcd1f250c3ad9da3b1a42168addd4b16429c3
tatiana-foliveira <tatianaferreiraoliveira@gmail.com>
2018-11-13 17:13:14 +0000

    setup

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  template: `
+    <h2>hello!</h2>
+    <img src="/assets/images/basic-shield.png" />
+    `,
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
