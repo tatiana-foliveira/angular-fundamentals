f20eb939ac98ff051d01ad56298d253f38ed212a
tatiana-foliveira <tatianaferreiraoliveira@gmail.com>
2018-11-14 16:04:53 +0000

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [
@@ -15,6 +17,10 @@ import { NavbarComponent } from './navbar/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavbarComponent
+  ],
+  providers: [
+    EventService,
+    ToastrService
   ],
   bootstrap: [EventsAppComponent]
 })
