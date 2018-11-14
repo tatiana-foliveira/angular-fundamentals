155fcca165d6a9af5d05e15ebbd90cc0bd7d1c54
tatiana-foliveira <tatianaferreiraoliveira@gmail.com>
2018-11-14 10:43:30 +0000

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-events',
   template: `
-    <h2>hello!</h2>
-    <img src="/assets/images/basic-shield.png" />
-    `,
+  <app-navbar></app-navbar>
+  <app-events-list></app-events-list>`
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
